6447ae2845cc3b01f471ad77369ee08a5646ad0c
andre-jorge <82469677+andre-jorge@users.noreply.github.com>
2022-05-14 18:06:47 +0100

    Login e Registo
    Ao fazer login somos redirecionados para a pagina inicial, o mesmo com o logout.
    Para isto renomeei o welcome.blade.php para home.blade.php.
    comentei o codigo do carrinho de compras e mudei o nome da rota criada automaticamente para _home.blade.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\FilmesController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
  Route::get('filmes', [FilmesController::class, 'index'])
