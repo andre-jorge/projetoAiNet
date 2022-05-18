d2d45a85ac752ac8aead4e2c39a3422946aa8e76
GabrielNeves24 <100974974+GabrielNeves24@users.noreply.github.com>
2022-05-18 21:51:49 +0100

    a
    a

--- resources/views/sessoes/index.blade.php
+++ resources/views/sessoes/index.blade.php
@@ -1,32 +1,26 @@
 @extends('home')
 
 @section('content')
-<div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-    <div class="col mb-5">
-        <div class="card h-100">
-            <div class="card-body p-4">
+<header class="bg-dark py-5">
+    <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+            <h1 class="display-4 fw-bolder">Filmes em Destaques</h1>
                 <div class="text-center" name="sess" id="idSess">
-                    
-
                     <form class="sess-search" action="#" method="GET">
                         <div class="search-item">
-                            <label for="idSess">Disc:</label>
-                            <div name="Fil" id="idFilme">
-                            @foreach ($FilmeSessoes as $fil)
-                            <a value="{{$fil->id}}">{{$fil->data}} - {{$fil->horario_inicio}}</a>
-                                
-                            @endforeach
-                            </div>
+                                <div name="Fil" id="idFilme">
+                                    @foreach ($FilmeSessoes as $fil)
+                                        <a value="{{$fil->id}}">{{$fil->data}} - {{$fil->horario_inicio}}</a>
+                                    @endforeach
+                                </div>
                         </div>
                         <div class="search-item">
                             <button type="submit" class="bt" id="btn-filter">Filtrar</button>
                         </div>
-                    </form>
-                                        
+                    </form>                          
                 </div>
-            </div>
-            <!-- Product actions-->
-            
+        </div>
+
         </div>
         </div>
     </div>
