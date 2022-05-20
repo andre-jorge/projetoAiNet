beb3673b7e408edebd396984463df1adc6d19954
GabrielNeves24 <100974974+GabrielNeves24@users.noreply.github.com>
2022-05-20 23:31:13 +0100

    Update

--- resources/views/sessoes/index.blade.php
+++ resources/views/sessoes/index.blade.php
@@ -1,11 +1,16 @@
 @extends('home')
 
 @section('content')
+<?php
+$url = $filme->trailer_url;
+preg_match('/[\\?\\&]v=([^\\?\\&]+)/', $url, $matches);
+$id = $matches[1];
+$width = '800px';
+$height = '450px'; ?>
 
 
 
-    <div class="card h-50">
-        <div class="card-body p-10">
+    <div class="card h-300">
             <div class="text-center">
                 
                     <div class="row">
@@ -19,12 +24,22 @@
                                 <span class="float-left"><?= $filme->ano ?> </span>
                             </div>
                         </div>
+                        <p>
+                        </p>
+                        <h4>TRAILER</h4>
+                        <div class="text-center">
+                        <iframe id="ytplayer" type="text/html" width="<?php echo $width ?>" height="<?php echo $height ?>"
+                            src="https://www.youtube.com/embed/<?php echo $id ?>?rel=0&showinfo=0&color=white&iv_load_policy=3"
+                            frameborder="0" allowfullscreen></iframe> 
+                        </div>
                     <table class="table">
                     <thead>
                         <tr>
                         
                         <th scope="col">Dia Sessao</th>
                         <th scope="col">Hora</th>
+                        <th scope="col">sala_id</th>
+                        <th scope="col">BILHETES TODOS COMPRADOS???</th>
                         <th scope="col">sala_id</th>
                         </tr>
                     </thead>
@@ -34,12 +49,13 @@
                         <td><?= $sessao->data ?></td>
                         <td><?= $sessao->horario_inicio ?></td>
                         <td><?= $sessao->nome ?></td>
+                        <td><?= $sessao->nome ?></td>
+                        <td><?= $sessao->nome ?></td>
                         </tr>
                         @endforeach
                     </tbody>
                     
             </div>
-        </div>
     </div>
 
 @endsection
