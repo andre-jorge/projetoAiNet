b7de0d3696925091e3afa8e2517066947c4fb08a
andre-jorge <82469677+andre-jorge@users.noreply.github.com>
2022-05-21 00:48:34 +0100

    LOGIN E PAGINA INICIAL
    LOGIN E PAGINA INICIAL C/ FILMES

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
     <body>
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html">CineDEI</a>
+            <a class="navbar-brand ps-3" href="home">CineDEI</a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search
@@ -115,7 +115,7 @@
                                 </nav>
                             </div>
                             <div class="sb-sidenav-menu-heading">Addons</div>
-                            <a class="nav-link" href="{{route('filmes.index')}}">
+                            <a class="nav-link" href="">
                                 <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
                                 Filmes
                             </a>
