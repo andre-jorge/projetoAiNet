8d65be506215a59a2e63dfddfbd0194e4aa44913
GabrielNeves24 <100974974+GabrielNeves24@users.noreply.github.com>
2022-05-20 23:13:18 +0100

    Revert "LOGIN ok"
    This reverts commit 966fb82f58ebf86c0df25ed5999333dcde0f9a65.

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     
+Auth::routes();
 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
