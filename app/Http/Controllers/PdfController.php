59a1aa5cb4a81a8a21edb1b1bc11e1dc74fd8ded
GabrielNeves24 <100974974+GabrielNeves24@users.noreply.github.com>
2022-06-16 16:53:14 +0100

    ++
    tive de tirar umas validacoes de admin e func

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Bilhetes;
 use App\Models\Recibo;
+use App\Models\User;
 use PDF;
 
 class PdfController extends Controller
@@ -34,10 +35,41 @@ class PdfController extends Controller
         return $pdf->setPaper('A4')->stream('recibo.pdf');
     }
 
-    public function indexRecibo()
+    public function indexRecibo(Request $request)
       {
-         $recibos = Recibo::paginate(8);
-         //dd($recibos);
-         return view('pdf.indexRecibo', compact('recibos'));
+        if (is_null($request->reciboid) and is_null($request->data) and is_null($request->clientenome) and is_null($request->nif) and is_null($request->tipo_pagamento)) {
+            $recibos = Recibo::paginate(8);
+            return view('pdf.indexRecibo', compact('recibos'));
+        }
+        
+        if ($request->reciboid != null) {
+            $recibos = Recibo::where('id',$request->reciboid)->paginate(8);
+            return view('pdf.indexRecibo', compact('recibos'));
+        }
+        //data
+        if ($request->data != null) {
+            $recibos = Recibo::where('data',$request->data)->paginate(8);
+            return view('pdf.indexRecibo', compact('recibos'));
+        }
+        //clientenome
+        if ($request->clientenome != null) {
+            $clienteid = User::where('name', 'like', '%' . $request->clientenome . '%')
+                        ->pluck('id');
+            $recibos = Recibo::where('cliente_id',$clienteid)->paginate(8);
+            return view('pdf.indexRecibo', compact('recibos'));
+        }
+        //nif
+        if ($request->nif != null) {
+            $recibos = Recibo::where('nif',$request->nif)->paginate(8);
+            return view('pdf.indexRecibo', compact('recibos'));
+        }
+        //nif
+        if ($request->tipo_pagamento != 'TODOS') {
+            $recibos = Recibo::where('tipo_pagamento',$request->tipo_pagamento)->paginate(8);
+            return view('pdf.indexRecibo', compact('recibos'));
+        }
+            $recibos = Recibo::paginate(8);
+               
+        return view('pdf.indexRecibo', compact('recibos'));
       }
 }
--- resources/views/pdf/indexRecibo.blade.php
+++ resources/views/pdf/indexRecibo.blade.php
@@ -1,18 +1,56 @@
 @extends('home')
 @section('content')
+<div class="container text-center"
+<h3 class="align-middle"><strong><h1 class="align-middle">Recibos Clientes</h1></strong></h3>
+</div>
+<br>
+<br>
+<form  action="{{route('pdf.indexRecibo')}}" method="GET">
+<div class="row mb-2">
+    <div class="col-sm-2 col-md-2">
+        <!-- reciboid -->
+        <input type="number" name="reciboid" id="reciboid" class="form-control rounded" placeholder="Numero Recibo" aria-label="Search" aria-describedby="search-addon" />    
+        <!-- ReciboID --> 
+    </div>
+    <div class="col-sm-2 col-md-2">
+        <!-- data -->
+        <input type="date" name="data" id="data" class="form-control rounded" placeholder="Data" aria-label="Search" aria-describedby="search-addon" />    
+        <!-- data --> 
+    </div>
+    <div class="col-sm-2 col-md-2">
+        <!-- clientenome -->
+        <input type="search" name="clientenome" id="clientenome" class="form-control rounded" placeholder="Nome Cliente" aria-label="Search" aria-describedby="search-addon" />    
+        <!-- clientenome --> 
+    </div>
+    <div class="col-sm-2 col-md-2">
+        <!-- nif -->
+        <input type="search" name="nif" id="nif" class="form-control rounded" placeholder="NIF" aria-label="Search" aria-describedby="search-addon" />    
+        <!-- nif --> 
+    </div> 
+    <div class="col-sm-2 col-md-2">
+        <!-- tipo_pagamento -->
+        <select class="form-select" name="tipo_pagamento" id="tipo_pagamento">
+                <option value="TODOS">Todos</option>
+                <option value="PAYPAL">Paypal</option>
+                <option value="MBWAY">MBWAY</option>
+                <option value="VISA">Visa</option>
+        </select>
+        <!-- tipo_pagamento -->
+    </div>  
+    <div class="col-sm-5 col-md-2 .ml-md-auto" >
+        <button type="submit" class="btn btn-outline-primary"><i class="fas fa-search"></i> Pesquisar</button>
+    </div>
+</div>
+</form> 
 <table class="table table-striped">
     <thead>
         <tr>
-            <th scope="col">Num_Recibo</th>
+            <th scope="col">Recibo</th>
             <th scope="col">Cliente</th>
-            <th scope="col">Data Recibo</th>
-            <th scope="col">Total s/Iva</th>
-            <th scope="col">Iva</th>
+            <th scope="col">Data </th>
             <th scope="col">Total c/Iva</th>
             <th scope="col">NIF</th>
-            <th scope="col">Nome Cliente</th>
             <th scope="col">Tipo Pagamento</th>
-            <th scope="col">Ref Pagamento</th>
             <th scope="col"></th>
             <th scope="col"></th>
         </tr>
@@ -21,18 +59,16 @@
         @foreach ($recibos as $recibo)
         <tr>
             <td>{{$recibo->id}}</td>
-            <td>{{$recibo->cliente_id}}</td>
+            <td>{{$recibo->nome_cliente}}</td>
             <td>{{$recibo->data}}</td>
-            <td>{{$recibo->preco_total_sem_iva}}</td>
-            <td>{{$recibo->iva}}</td>
             <td>{{$recibo->preco_total_com_iva}}</td>
             <td>{{$recibo->nif}}</td>
-            <td>{{$recibo->nome_cliente}}</td>
             <td>{{$recibo->tipo_pagamento}}</td>
-            <td>{{$recibo->ref_pagamento}}</td>
-            <td><a href="{{ route('pdf.recibo', $recibo->id ) }}" name="recibo" value='{{$recibo}}' class="btn btn-info" role="button" aria-pressed="true">Recibo</a>
+            <td>
+                <a href="{{ route('pdf.recibo', $recibo->id ) }}" name="recibo" value='{{$recibo}}' class="btn btn-info" role="button" aria-pressed="true">Recibo</a>
             </td>
-            <td><a href="{{ route('pdf.bilhete', $recibo->id ) }}" name="bilhetes" value='{{$recibo}}' class="btn btn-info" role="button" aria-pressed="true">Bilhete(s)</a>
+            <td>
+                <a href="{{ route('pdf.bilhete', $recibo->id ) }}" name="bilhetes" value='{{$recibo}}' class="btn btn-info" role="button" aria-pressed="true">Bilhete(s)</a>
             </td>
         </tr>
         @endforeach
